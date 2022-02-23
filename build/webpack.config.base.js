a54c1c943ef10ddb7a7b382e45e81cf1868fa66a
樊宗领 <fanzongling@up366.com>
2022-02-23 22:51:21 +0800

    add html-webpack-plugin

--- build/webpack.config.base.js
+++ build/webpack.config.base.js
@@ -1,3 +1,17 @@
+const { resolve } = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const cwd = process.cwd();
 module.exports = {
-    entry: './src/index.js'
+    entry: './src/index.js',
+    output: {
+        filename: 'js/app.[contenthash:10].js',
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            template: 'public/index.html',
+            title: '目录'
+        })
+    ],
 }
