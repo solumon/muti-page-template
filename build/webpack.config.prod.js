8695c4bfc4efe974a271d344f3063b6fee718d5f
樊宗领 <fanzongling@up366.com>
2022-02-23 22:31:24 +0800

    add webpack config

--- build/webpack.config.prod.js
+++ build/webpack.config.prod.js
@@ -1,5 +1,5 @@
 const { merge } = require('webpack-merge');
-const base = require('webpack.config.base.js');
+const base = require('./webpack.config.base.js');
 
 module.exports = merge(base, {
     mode: "production"
